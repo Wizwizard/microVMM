35d78a758758314db9b735409200776cb8a41e2c
BingxingWang <303704668@qq.com>
2022-04-13 20:48:17 -0500

    getch

--- kvmtest.c
+++ kvmtest.c
@@ -103,17 +103,27 @@ int kbhit()
     return select(1, &fds, NULL, NULL, NULL) > 0;
 }
 
-int getch()
+char getch(void)
 {
-    int r;
-    unsigned char c;
-    if ((r = read(0, &c, sizeof(c))) < 0) {
-        return r;
-    } else {
-        return c;
-    }
+    char buf = 0;
+    struct termios old = {0};
+    fflush(stdout);
+    if(tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if(tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if(read(0, &buf, 1) < 0)
+        perror("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if(tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsetattr ~ICANON");
+    return buf;
 }
-
 
 int main() {
     int kvm;
