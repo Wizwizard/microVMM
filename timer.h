d143818da352fd37bb03eefeef2873cfa9c76810
BingxingWang <303704668@qq.com>
2022-04-13 23:42:57 -0500

    timer

--- timer.h
+++ timer.h
@@ -1,11 +1,11 @@
 #include <time.h>
 
-typedef struct timer {
+typedef struct kvm_timer {
     long interval_ms;
     long last_fired_ms;
     int timer_enable;
-} timer_t;
+} kvm_timer_t;
     
 
-int timer_should_fire(timer_t *timer);
-void timer_update(timer_t *timer);
+int timer_should_fire(kvm_timer_t *timer);
+void timer_update(kvm_timer_t *timer);
